5df6d2c1a3e94dc55fe6973cb8f090e363688ca8
Đặng Anh Tú <tuda@talent.ominext.com>
2020-02-25 08:51:16 +0700

    1212

--- FUNCTION/function.sql
+++ FUNCTION/function.sql
@@ -5,6 +5,7 @@ AS
 BEGIN
 	DECLARE @MSV varchar(6);
 	SET @MSV = @StudentID;
+
 	DECLARE @count_StudentID int;
 	SET @count_StudentID = (SELECT COUNT(S.StudentID) FROM QLSV.dbo.Student as S WHERE S.StudentID = @MSV)
 	IF @count_StudentID <= 0
@@ -36,12 +37,77 @@ BEGIN
 		SET @result = -1
 	ELSE
 		BEGIN
-			SET @result = (SELECT Avg(Grade) FROM QLSV.dbo.Grade WHERE StudentID = @MSV)
+			SET @result = ((SELECT Sum(Grade) FROM QLSV.dbo.Grade WHERE StudentID = @MSV)/dbo.getSumCredit('A00003'))
 		END
 	RETURN @result
 END
 
 print 'This student (A00003) has GPA=' + cast(dbo.getAverageGrade('A00003') as varchar(max))
+
+
+
+
+-- FUNCTION GET AVERAGE GRADE BY MSV
+ALTER FUNCTION getAverageGradeWithCredit(@StudentID varchar(6))
+RETURNS float
+AS
+BEGIN
+	DECLARE @MSV varchar(6);
+	SET @MSV = @StudentID;
+
+	DECLARE @count_CourseId_AP1 int;
+	DECLARE @count_CourseId_AP2 int;
+
+	DECLARE @temp TABLE(CourseID int);
+	INSERT INTO @temp SELECT CourseID FROM QLSV.dbo.Grade WHERE StudentID = @MSV
+
+	SET @count_CourseId_AP1 = (
+		SELECT COUNT(C.CourseID) FROM AP1.QLSV.dbo.Course as C INNER JOIN @temp as T 
+		ON C.CourseID = T.CourseID  WHERE C.CourseID <= 5
+		)
+	SET @count_CourseId_AP2 = (
+		SELECT COUNT(C.CourseID) FROM AP2.QLSV.dbo.Course as C INNER JOIN @temp as T 
+		ON C.CourseID = T.CourseID WHERE C.CourseID > 5
+		)
+
+	DECLARE @count_StudentID int;
+	SET @count_StudentID = (SELECT COUNT(S.StudentID) FROM QLSV.dbo.Student as S WHERE S.StudentID = @MSV)
+
+	DECLARE @result float;
+
+	IF @count_StudentID <= 0
+		SET @result = -1
+	ELSE
+		BEGIN
+			IF (@count_CourseId_AP1 > 0 AND @count_CourseId_AP2 > 0)
+				BEGIN
+					DECLARE @result1 int;
+					DECLARE @result2 int;
+					SET @result1 = (SELECT Sum(Grade.Grade*Course.Credit) FROM AP1.QLSV.dbo.Grade INNER JOIN AP1.QLSV.dbo.Course 
+									ON Grade.CourseID = Course.CourseID WHERE StudentID = @MSV)
+					SET @result2 = (SELECT Sum(Grade.Grade*Course.Credit) FROM AP2.QLSV.dbo.Grade INNER JOIN AP2.QLSV.dbo.Course 
+									ON Grade.CourseID = Course.CourseID WHERE StudentID = @MSV)
+					SET @result = (@result1 + @result2)/dbo.getSumCredit(@MSV)
+					
+				END
+			ELSE
+				BEGIN
+					IF @count_CourseId_AP2 <= 0
+						BEGIN
+							SET @result = (SELECT Sum(Grade.Grade*Course.Credit) FROM AP1.QLSV.dbo.Grade INNER JOIN AP1.QLSV.dbo.Course 
+									ON Grade.CourseID = Course.CourseID WHERE StudentID = @MSV)/dbo.getSumCredit(@MSV)
+						END
+					ELSE
+						BEGIN
+							SET @result = (SELECT Sum(Grade.Grade*Course.Credit) FROM AP2.QLSV.dbo.Grade INNER JOIN AP2.QLSV.dbo.Course 
+									ON Grade.CourseID = Course.CourseID WHERE StudentID = @MSV)/dbo.getSumCredit(@MSV)
+						END
+				END
+		END
+	RETURN @result
+END
+
+print 'This student (A00003) has GPA=' + cast(dbo.getAverageGradeWithCredit('A00003') as varchar(max))
 
 
 
