d8558d401aa17b97c23145a029b160d8d38fd6bd
Termanteus <bomcon123456@gmail.com>
2020-02-25 09:04:13 +0700

    Fix_TC_func

--- TEST_SCENARIO/function.sql
+++ TEST_SCENARIO/function.sql
@@ -4,16 +4,24 @@ GO
 -- Get grade By MSV
   -- Success
   SELECT * FROM getGrade('A00001');
-
+  -- Not exist
+  SELECT * FROM getGrade('INVALID')
 
 -- Get average grade By MSV
 	-- Success 
-	print dbo.getAverageGrade('A00003')
-	-- Invalid StudentID
+	print 'This student (A00003) has GPA=' + cast(dbo.getAverageGrade('A00003') as varchar(max))
+	-- Invalid StudentID will return -1
 	print dbo.getAverageGrade('A00005')
+
+-- Get avg grade with credits by msv
+	-- Success 
+	print 'This student (A00003) has GPA=' + cast(dbo.getAverageGradeWithCredit('A00003') as varchar(max))
+	-- Invalid StudentID will return -1
+	print dbo.getAverageGrade('A00005')
+
 
 -- Get sum credit by MSV
   -- Success
-  print dbo.getSumCredit('A00003')
+  print 'This student (A00003) has gathered total of ' + cast(dbo.getSumCredit('A00003') as varchar(max)) + ' credits.'
   -- Invalid StudentID
   print dbo.getSumCredit('A00005')
